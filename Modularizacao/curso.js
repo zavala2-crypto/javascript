875cd1b46a2080e67447f00b42340dbcebf5ac7b
Alberto Zavala <albertotomas701@gmail.com>
2026-02-22 06:56:28 -0800

    modularizacao

---
+++ Modularizacao/curso.js
@@ -0,0 +1,4 @@
+const curso = ["JavaScript", "HTML", "CSS", "Arduino","Raspnerry", "Java"]
+const carros = ["Polo", "Golf", "Mazda", "TOYOTA"]
+
+export {curso}
---
+++ Modularizacao/mod.js
@@ -0,0 +1,5 @@
+//Modulo e uma organizacao e distruicao de codigo
+
+import { curso } from "./curso.js"
+console.log(curso)
+
